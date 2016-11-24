db7fec74e2e72387c3d283b7214a2dcac256fa9c
Alexander <stwork87@gmail.com>
2016-11-24 19:16:18 +0300

    после объединения

--- js/jscripts.js
+++ js/jscripts.js
@@ -9,6 +9,10 @@ var link=document.querySelector(".we-stray-button");
     var location_link=document.querySelector(".small-map");
     var big_map=document.querySelector(".modal-map");
     var close_map=document.querySelector(".modal-map>.window-close-button");
+    var cartOpen = document.querySelectorAll(".buy-item");
+    var cartPopup = document.querySelector(".goods-buying-notification");
+    var cartClose = cartPopup.querySelector(".goods-buying-notification>.window-close-button");
+    var cartContinue = cartPopup.querySelector(".make-order");
     
 
     link.addEventListener("click", function(event){
@@ -61,4 +65,30 @@ var link=document.querySelector(".we-stray-button");
       console.log("3");
       big_map.classList.remove("map_ap");
     });
-    
+
+    if (cartOpen && cartPopup && cartClose) {
+      for (var i=0; i<cartOpen.length; i++) {
+          cartOpen[i].addEventListener("click", function (event) {
+          event.preventDefault();
+          cartPopup.classList.add("notification_ap");
+          });
+      }
+
+      cartClose.addEventListener("click", function (event) {
+      event.preventDefault();
+      cartPopup.classList.remove("notification_ap");
+      });
+
+      cartContinue.addEventListener("click", function(event) {
+      event.preventDefault();
+      cartPopup.classList.remove("notification_ap");
+      });
+
+      window.addEventListener("keydown", function (event) {
+        if (event.keyCode === 27) {
+          if (cartPopup.classList.contains("modal-content-show")) {
+          cartPopup.classList.remove("notification_ap");
+          }
+        }
+      });
+    }
