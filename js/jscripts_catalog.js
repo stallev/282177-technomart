5c5ca0bce45ca80934c0da2e15db55930d555a02
Alexander <stwork87@gmail.com>
2016-11-24 18:49:01 +0300

    на проверку

--- js/jscripts_catalog.js
+++ js/jscripts_catalog.js
@@ -1,15 +1,31 @@
-    var notification_link=document.querySelector(".buy-item");
-    var notification=document.querySelector(".goods-buying-notification");
-    var close_notification=document.querySelector(".goods-buying-notification>.window-close-button");
+var cartOpen = document.querySelectorAll(".buy-item");
+var cartPopup = document.querySelector(".goods-buying-notification");
+var cartClose = cartPopup.querySelector(".goods-buying-notification>.window-close-button");
+var cartContinue = cartPopup.querySelector(".make-order")
 
-    notification_link.addEventListener("click", function(event){
+if (cartOpen && cartPopup && cartClose) {
+  for (var i=0; i<cartOpen.length; i++) {
+      cartOpen[i].addEventListener("click", function (event) {
       event.preventDefault();
-      console.log("Кликнули по карточке товара");
-      notification.classList.add("notification_ap");
-      console.log("8");
-    });
-    close_notification.addEventListener("click", function(event){
-      event.preventDefault();
-      console.log("9");
-      notification.classList.remove("notification_ap");
-    });
+      cartPopup.classList.add("notification_ap");
+      });
+  }
+
+  cartClose.addEventListener("click", function (event) {
+  event.preventDefault();
+  cartPopup.classList.remove("notification_ap");
+  });
+
+  cartContinue.addEventListener("click", function(event) {
+  event.preventDefault();
+  cartPopup.classList.remove("notification_ap");
+  });
+
+  window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+      if (cartPopup.classList.contains("modal-content-show")) {
+      cartPopup.classList.remove("notification_ap");
+      }
+    }
+  });
+}
